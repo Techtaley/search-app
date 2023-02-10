91d67c6c98f7932ee1294202e5eeab84d3189d5d
expansivedesign <taloveous@expansivedesigns.com>
2023-02-09 23:22:12 -0500

    update nav-links

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,7 +4,7 @@ import { NavLink, Link } from 'react-router-dom'
 export default function MainNav() {
   return (
     <header className="vertical-center">
-        <Link to="/search-app"><h1>CRUD App</h1></Link>
+        <Link className="nav-links" to="/search-app"><h1>CRUD App</h1></Link>
         <nav className="nav-flex">
           <NavLink className="nav-links" to="/posts">posts</NavLink>
           <NavLink className="nav-links" to="/about">about</NavLink>
